9a3ae0d4c0e6ff440ed22e68d0f10c4db7534759
Tim Arendsen <t.j.m.arendsen@gmail.com>
2024-07-05 19:18:44 +0200

    Implemented Juffen explanation

--- src/assets/minigames.js
+++ src/assets/minigames.js
@@ -3,7 +3,7 @@ export default [
     {
         description: {
             en: 'TEST EN',
-            nl: 'TEST NL',
+            nl: 'Wie kent het niet, juffen? Tel tot 70, maar vergeet niet: elke keer als je 7, een dubbel getal (11, 22, ...) of een getal in de tafel van 7 op moet noemen zeg je "JUF". Hopelijk zijn jullie een beetje hoogbegaafd.',
         },
         emoji: '💯',
         title: {
